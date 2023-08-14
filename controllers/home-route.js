25de149d6ef9a7d4421bc62d05fa25d9284d14b5
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-08-13 22:40:47 -0400

    begin routes, flesh out server.js, add renderposts.handlebars

---
+++ controllers/home-route.js
@@ -0,0 +1,71 @@
+const router = require('express').Router();
+const sequelize = require('sequelize');
+const { Comment, Post, User } = require('../models');
+
+// GET all galleries for homepage
+router.get('/', async (req, res) => {
+  try {
+    const dbPosts = await Post.findAll({
+        attributes: ['id, post_title, post_body'],
+        include: [
+        {
+          model: Comment,
+          attributes: ['id, comment_body, user_id, post_id'],
+          include: {
+            model: User,
+            attributes: ['username']
+          },
+        },
+        {
+            model: User,
+            attributes: ['name']
+        },
+      ],
+    });
+
+    const posts = dbPosts.map((post) =>
+      post.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+router.get('/post/:id', async (req, res) => {
+  try {
+    const dbPosts = await Post.findOne(req.params.id, {
+        where: {id: req.params.id},
+        attributes: ['id, post_title, post_body'],
+        include: [
+            {
+              model: Comment,
+              attributes: ['id, comment_body, user_id, post_id'],
+              include: {
+                model: User,
+                attributes: ['username']
+              },
+            },
+            {
+                model: User,
+                attributes: ['name']
+            },
+          ],
+    });
+
+    const post = dbPosts.get({ plain: true });
+    res.render('renderposts', { post, logged_in: req.session.logged_in });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-const { Sequelize } = require('sequelize');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +16,7 @@ const sesn = {
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-        db: Sequelize
+        db: sequelize
     }),
 };
 
@@ -23,6 +25,10 @@ app.use(session(sesn));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+const hbs = exphbs.create({helpers});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => {
